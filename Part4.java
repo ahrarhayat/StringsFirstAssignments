88837490d9150d28475235e88e3a00375621b28e
ahrarhayat <ahrarhayat@gmail.com>
2019-02-24 13:00:09 +0600

    [FIX] Fixed a lot of issues

--- Part4.java
+++ Part4.java
@@ -11,18 +11,38 @@ public class Part4 {
     {
        
       
-       String searchtext="youtube.com";
+       
        String results="null";
        for(String words:url.words())
        {
-          String itemLower= words.toLowerCase();
-          int pos= itemLower.indexOf("youtube.com");
+          String itemLower= words;
+          String youtubeUp= "YouTube.com";
+          String youtubeLo= "youtube.com";
+          String youtubeUpful="YOUTUBE";
+       
+          int pos= words.indexOf(youtubeLo);
+          int pos2=words.indexOf(youtubeUp);
+          int pos3=words.indexOf(youtubeUpful);
+          
+          
          
-           if(pos!=-1)
+           if(pos!=-1 && words.contains("youtube.com")== true)
            {
                int beg = itemLower.lastIndexOf("\"",pos);
                int end = itemLower.indexOf("\"",pos+1);
                System.out.println(itemLower.substring(beg+1,end));
+            }
+           else if(pos!=-1 && words.contains("YouTube.com")== true)
+            {
+               int beg = itemLower.lastIndexOf("\"",pos2);
+               int end = itemLower.indexOf("\"",pos2+1);
+               System.out.println(itemLower.substring(beg+1,end));
+            }
+           else if(pos!=-1 && words.contains("YOUTUBE")== true)
+            {
+             int beg = itemLower.lastIndexOf("\"",pos3);
+              int end = itemLower.indexOf("\"",pos3+1);
+              System.out.println(itemLower.substring(beg+1,end));   
             }
             
            
@@ -35,7 +55,7 @@ public class Part4 {
     }
     public void test()
     {
-        URLResource url = new URLResource("http://www.dukelearntoprogram.com/course2/data/manylinks.html");
+        URLResource url = new URLResource("http://www.dukelearntoprogram.com/course2/data/manylinks.html ");
      
         String results=FindYoutube(url);
         System.out.println(results);
